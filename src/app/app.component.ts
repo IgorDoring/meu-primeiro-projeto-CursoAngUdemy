f78e0e3856586049e14a8a41d007b53a6bf780ab
IgorDoring <idoring@tutanota.com>
2023-03-23 00:29:13 -0300

    Section 3: ngDoCheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-title title="New Value"></app-title>
+    {{ valor }}
+    <button (click)="adicionar()">Adicionar</button>
     <router-outlet></router-outlet>
   `,
 })
 export class AppComponent implements OnInit{
+  public valor: number = 1;
+
+  public adicionar(): number{
+    return this.valor += 1;
+  }
+
   contructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.valor = 0;
+  }
 
+  ngDoCheck(): void{
+    console.log("ngDoCheck")
+  }
+
+  ngAfterContentInit(): void{
+    console.log("ngAfterContentInit")
+  }
+  ngAfterContentChecked(): void{
+    console.log("ngAfterContentChecked")
+  }
+  ngAfterViewInit(): void{
+    console.log("ngAfterViewInit")
+  }
+  ngAfterViewChecked(): void{
+    console.log("ngAfterViewChecked")
+  }
 }
--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,18 +1,20 @@
-import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit, OnChanges{
+export class TitleComponent{
 
   @Input() public title:string = "Hello World!";
 
-  ngOnInit(): void{ }
-
-  ngOnChanges(): void{ 
-    console.log("New Data");
+  ngOnInit(): void{
+    console.log("Title Component Initialiazed");
   }
 
+  ngOnChanges(): void{ 
+    console.log("Title New Data");
+  }
+  
 }
