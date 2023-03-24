0656d53c50355acd73b8d5b00e65a445983a95b3
IgorDoring <idoring@tutanota.com>
2023-03-24 01:02:40 -0300

    Section 4: interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-title *ngIf="!destruir"></app-title>
-    <button (click)="destruirCompo()">Destruir Componente</button>
+  <app-data-binding></app-data-binding>
     <router-outlet></router-outlet>
   `,
 })
 export class AppComponent implements OnInit{
-  
-  public destruir: boolean = false;
 
   contructor() {}
 
   ngOnInit(): void {}
 
-  public destruirCompo(){
-    this.destruir = true;
-  }
 }
