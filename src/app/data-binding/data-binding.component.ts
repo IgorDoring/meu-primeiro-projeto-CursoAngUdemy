647cafda03617171aed42c137591b18bc98af3bd
IgorDoring <idoring@tutanota.com>
2023-03-24 15:36:39 -0300

    Section 4: event binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -15,4 +15,15 @@ export class DataBindingComponent {
   /* Property Binding*/
   public disabledCheck: boolean = true;
   public imgSrc: string = "https://www.unisinos.br/images/logos/unisinos-logo.svg"
+
+  // Event Binding
+  public position: {x: number, y: number} = {x: 0, y: 0};
+  alertaInfo(valor: MouseEvent): void{
+    console.log(valor);
+  }
+
+  mouseMoveTeste(valor: MouseEvent): void{
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 }
