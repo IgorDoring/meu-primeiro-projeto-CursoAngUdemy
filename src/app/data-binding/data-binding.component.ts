0079ad21dc9988f7582fc233ebe0316bff731e9e
IgorDoring <idoring@tutanota.com>
2023-03-24 01:17:57 -0300

    Section 4: property binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -7,8 +7,12 @@ import { Component } from '@angular/core';
 })
 export class DataBindingComponent {
 
+  /* Interpolation*/
   public nome: string = "Igor";
   public sobrenome: string = " Doring";
   public idade: number = 22;
 
+  /* Property Binding*/
+  public disabledCheck: boolean = true;
+  public imgSrc: string = "https://www.unisinos.br/images/logos/unisinos-logo.svg"
 }
