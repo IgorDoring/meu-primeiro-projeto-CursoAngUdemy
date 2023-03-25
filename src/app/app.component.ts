2a3231edf2a2a8c1ab41b34fbb9aef83e3a533d2
IgorDoring <idoring@tutanota.com>
2023-03-25 03:32:54 -0300

    Section 5: ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-diretivas-atributos></app-diretivas-atributos>
+  <app-diretivas-atributos><h1>Aulas de Diretiva Atributo</h1> <h3>Fim da Aula</h3></app-diretivas-atributos>
     <router-outlet></router-outlet>
   `,
 })
