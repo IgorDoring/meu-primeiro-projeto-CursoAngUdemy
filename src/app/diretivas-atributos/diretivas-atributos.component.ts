61bb5b2756636f6a90ec85d0003b540fa8892194
IgorDoring <idoring@tutanota.com>
2023-03-25 03:06:11 -0300

    Section 5: ngStyle

--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -6,10 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./diretivas-atributos.component.css']
 })
 export class DiretivasAtributosComponent {
-
+  // ngClass
   valor: boolean = true;
   changeValor(valor: boolean):void{
     this.valor = !this.valor;
   }
 
+  // ngStyle
+  paraHeight: number = 20;
+  ngOnInit(): void{
+    setInterval(()=>{
+      if(this.paraHeight < 50){
+        this.paraHeight += 2;
+      }else{
+        this.paraHeight = 20;
+      }
+    }, 100);
+  }
 }
