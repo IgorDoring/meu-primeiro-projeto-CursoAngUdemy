32cc9504f460bff4b6d7ae80d3dfa6e0bdac215f
IgorDoring <idoring@tutanota.com>
2023-03-24 22:14:55 -0300

    Section 5: ngFor

--- src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -6,9 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./diretivas-estruturais.component.css']
 })
 export class DiretivasEstruturaisComponent {
+  //ngIf 
   condition: boolean = true;
-
   changeCondition():void{
     this.condition = !this.condition;
   }
+
+  // ngFor
+  list: Array<{nome:string, descricao:string}> = [
+    {nome: "item 1", descricao: "desc 1"}, 
+    {nome: "item 2", descricao: "desc 2"}, 
+    {nome: "item 3", descricao: "desc 3"}, 
+    {nome: "item 4", descricao: "desc 4"}, 
+  ]
+
+  onClickAddList(): void{
+    this.list.push({nome: "item x", descricao: "desc x"});
+  }
+
+  onClickEvent(event: number): void{
+    this.list.splice(event, 1);
+  }
 }
+ 
